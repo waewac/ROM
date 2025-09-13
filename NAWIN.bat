0175f66196bc5aa173068754444b27db94a288af
waewac <152389095+waewac@users.noreply.github.com>
2025-09-13 17:37:32 +0700

    Create NAWIN.bat

---
+++ NAWIN.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+cd \dll\kaos\jack\hee\hdmi\bbox\bin
+cls
+wget https://github.com/waewac/ROM/raw/refs/heads/main/unlock.-natwn.bat
+cls
+start https://www.facebook.com/wsewac
+unlock.-natwn.bat
